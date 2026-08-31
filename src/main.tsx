e8e745559e885b8fb6ae2031fec72da6cfa8897f
begonia-474 <wengshizhuang@gmail.com>
2026-08-31 20:58:56 +0800

    feat: 添加 Noto Sans SC 字体支持，更新全局样式以使用新字体

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./i18n";
 import { applyTheme, loadTheme } from "./theme";
+import "@fontsource/noto-sans-sc/latin-400.css";
+import "@fontsource/noto-sans-sc/latin-500.css";
+import "@fontsource/noto-sans-sc/latin-600.css";
+import "@fontsource/noto-sans-sc/latin-700.css";
+import "@fontsource/noto-sans-sc/latin-800.css";
+import "@fontsource/noto-sans-sc/latin-900.css";
+import "@fontsource/noto-sans-sc/chinese-simplified-400.css";
+import "@fontsource/noto-sans-sc/chinese-simplified-500.css";
+import "@fontsource/noto-sans-sc/chinese-simplified-600.css";
+import "@fontsource/noto-sans-sc/chinese-simplified-700.css";
+import "@fontsource/noto-sans-sc/chinese-simplified-800.css";
+import "@fontsource/noto-sans-sc/chinese-simplified-900.css";
 import "./styles/global.css";
 
 applyTheme(loadTheme());
